f28b7d455fc321bd41def85d8808b83da48dde08
RaulMurillo <ramurimontero@gmail.com>
2024-02-12 01:06:46 +0100

    VSCode extensions

---
+++ install-vscode-extensions.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Check if the extensions file exists
+if [ ! -f "code_extensions" ]; then
+    echo "Error: Extensions file 'code_extensions' not found."
+    exit 1
+fi
+
+install_extensions() {
+    local program="$1"
+    # Check if the program exists
+    if command -v $program &> /dev/null; then
+        # Print start message
+        echo "$(date): Starting $program_name extension installation process"
+
+        # Read extensions from the file and install them
+        while IFS= read -r extension; do
+            # echo "$(date): Installing extension $extension"
+            $program_name --install-extension "$extension"
+            if [ $? -eq 0 ]; then
+                echo "$(date): Successfully installed extension $extension"
+            else
+                echo "$(date): Failed to install extension $extension"
+            fi
+        done < "code_extensions"
+
+        # Print completion message
+        echo "$(date): $program_name extension installation process completed"
+    else
+        echo "$program is not installed."
+    fi
+}
+
+# Perform installation
+install_extensions "code"
+install_extensions "$HOME/.vscode-server/bin/*/bin/code-server"
