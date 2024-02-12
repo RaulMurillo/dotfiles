b6098a04516a57de25a17da8e3a9ea113eb058cf
RaulMurillo <ramurimontero@gmail.com>
2024-02-12 01:42:56 +0100

    Fixed error with vscode extensions

--- install-vscode-extensions.sh
+++ install-vscode-extensions.sh
@@ -6,26 +6,28 @@ if [ ! -f "code_extensions" ]; then
     exit 1
 fi
 
+export NODE_OPTIONS=--throw-deprecation
+
 install_extensions() {
     local program="$1"
     # Check if the program exists
     if command -v $program &> /dev/null; then
         # Print start message
-        echo "$(date): Starting $program_name extension installation process"
+        echo "$(date): Starting $program extension installation process"
 
         # Read extensions from the file and install them
         while IFS= read -r extension; do
             # echo "$(date): Installing extension $extension"
-            $program_name --install-extension "$extension"
+            $program --install-extension "$extension"
             if [ $? -eq 0 ]; then
-                echo "$(date): Successfully installed extension $extension"
+                echo "✓ $(date): Successfully installed extension $extension"
             else
-                echo "$(date): Failed to install extension $extension"
+                echo "✗ $(date): Failed to install extension $extension"
             fi
         done < "code_extensions"
 
         # Print completion message
-        echo "$(date): $program_name extension installation process completed"
+        echo "$(date): $program extension installation process completed"
     else
         echo "$program is not installed."
     fi
